393b858967e4953dedb734dedf648f263d7b0b47
lexmarcos <marcosuel.vieira@hotmail.com>
2022-06-13 00:46:25 -0300

    feature(API): Muda a logica de criação do produtos dos filmes

--- api/movienight/pages/api/stores/index.ts
+++ api/movienight/pages/api/stores/index.ts
@@ -19,12 +19,8 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       return res.status(401).json({ message: 'Unauthorized' });
     }
     const db = (await clientPromise).db();
-    const { movieID, item } = req.body as IStoreItemBody;
-    await db.collection('stores').updateOne(
-    { movieID: movieID }, 
-    { $push: { products: item } },
-    { upsert: true }
-    );
+    const item = req.body as IStoreItemBody;
+    await db.collection('products').insertOne(item)
 
     return res.status(201).json({ message: 'Item added' });
   }
@@ -36,12 +32,15 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const { movieID } = req.query;
     const db = (await clientPromise).db();
     
-    const store = await db.collection('stores').findOne({ movieID: new Int32(movieID as string) }) as unknown as IStoreItem;
-    if(store === null){
-      return res.status(200).json({ store: {
+    const products = await db.collection('products').find({ movieID: new Int32(movieID as string) }).toArray() as unknown as IStoreItem[];
+    console.log(products);
+    if(products === null){
+      return res.status(200).json({ products: {
         products: []
       } })
     }
-    return res.status(200).json({ store });
+    return res.status(200).json({ store: {
+      products: products
+    } });
   }
 }
