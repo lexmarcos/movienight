2fcc906ae862b2ee709fcdf39e4c6d0a001854fa
henrymedeiros <henrymedeiros77@gmail.com>
2022-06-12 21:30:03 -0300

    Carrinho funcionalidades principais

--- app/lib/models/product.dart
+++ app/lib/models/product.dart
@@ -4,12 +4,24 @@ class Product {
   final double price;
   final String imageUrl;
 
-  const Product(this.id, this.name, this.price, this.imageUrl);
+  const Product(
+      {required this.id,
+      required this.name,
+      required this.price,
+      required this.imageUrl});
 
   static const List<Product> products = [
-    Product(1, "Camisa Dr.Estrenho", 24.99,
-        "https://img.elo7.com.br/product/zoom/2B8AF66/camiseta-doutor-estranho-dr-strange-namorados.jpg"),
-    Product(2, "Caneca Dr.Estranho", 14.99,
-        "https://static3.tcdn.com.br/img/img_prod/460977/caneca_doctor_strange_doutor_estranho_heroi_disney_preta_ev_91797_1_18c5e3dca2c6ecc535e0358c74f4eca8.jpg"),
+    Product(
+        id: 1,
+        name: "Camisa Dr.Estrenho",
+        price: 24.99,
+        imageUrl:
+            "http://https://img.elo7.com.br/product/zoom/2B8AF66/camiseta-doutor-estranho-dr-strange-namorados.jpg"),
+    Product(
+        id: 2,
+        name: "Caneca Dr.Estranho",
+        price: 14.99,
+        imageUrl:
+            "https://static3.tcdn.com.br/img/img_prod/460977/caneca_doctor_strange_doutor_estranho_heroi_disney_preta_ev_91797_1_18c5e3dca2c6ecc535e0358c74f4eca8.jpg"),
   ];
 }
--- app/lib/screens/movie_products_screen.dart
+++ app/lib/screens/movie_products_screen.dart
@@ -1,53 +1,109 @@
 import 'package:flutter/material.dart';
+import 'package:movienight/models/CartStore.dart';
 import 'package:movienight/models/product.dart';
 import 'package:movienight/utils/app_routes.dart';
+import 'package:provider/provider.dart';
 
-class MovieProductsScreen extends StatelessWidget {
+class MovieProductsScreen extends StatefulWidget {
   const MovieProductsScreen({Key? key}) : super(key: key);
 
+  @override
+  State<MovieProductsScreen> createState() => _MovieProductsScreenState();
+}
+
+class _MovieProductsScreenState extends State<MovieProductsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
           title: Text('Movie Products'),
         ),
-        body: Column(
-          children: [
-            Flexible(
-              child: ListView.builder(
-                  shrinkWrap: true,
-                  itemCount: Product.products.length,
-                  itemBuilder: ((context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 20, vertical: 10),
-                      child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            CircleAvatar(
-                                radius: 40,
-                                backgroundImage: NetworkImage(
-                                    Product.products[index].imageUrl)),
-                            Text(Product.products[index].name),
-                            Text('R\$ ${Product.products[index].price}'),
-                            IconButton(
-                                onPressed: () {
-                                  ScaffoldMessenger.of(context).showSnackBar(
-                                      SnackBar(
-                                          content: Text(
-                                              'Adicionou produto ${Product.products[index].name} ao carrinho')));
-                                },
-                                icon: Icon(Icons.add_circle))
-                          ]),
-                    );
-                  })),
-            ),
-            ElevatedButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed(AppRoutes.CART);
-                },
-                child: Text('Check Cart'))
-          ],
+        body: Center(
+          child: Column(
+            children: [
+              Flexible(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: Product.products.length,
+                    itemBuilder: ((context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CircleAvatar(
+                                  radius: 40,
+                                  backgroundImage: NetworkImage(
+                                      Product.products[index].imageUrl)),
+                              Text(Product.products[index].name),
+                              Text('R\$ ${Product.products[index].price}'),
+                              IconButton(
+                                  onPressed: () {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                        SnackBar(
+                                            content: Text(
+                                                'Adicionou produto ${Product.products[index].name} ao carrinho')));
+                                    final product = context.read<CartStore>();
+                                    product.addToCart(Product.products[index]);
+                                  },
+                                  icon: Icon(Icons.add_circle))
+                            ]),
+                      );
+                    })),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(AppRoutes.CART);
+                  },
+                  child: Text('Check Cart'))
+            ],
+          ),
         ));
   }
 }
+
+/* 
+Consumer<CartStore>(builder: (context, cart, child) {
+          return Text("${cart.counter}");
+        }));
+Center(
+          child: Column(
+            children: [
+              Flexible(
+                child: ListView.builder(
+                    shrinkWrap: true,
+                    itemCount: Product.products.length,
+                    itemBuilder: ((context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.symmetric(
+                            horizontal: 20, vertical: 10),
+                        child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              CircleAvatar(
+                                  radius: 40,
+                                  backgroundImage: NetworkImage(
+                                      Product.products[index].imageUrl)),
+                              Text(Product.products[index].name),
+                              Text('R\$ ${Product.products[index].price}'),
+                              IconButton(
+                                  onPressed: () {
+                                    ScaffoldMessenger.of(context).showSnackBar(
+                                        SnackBar(
+                                            content: Text(
+                                                'Adicionou produto ${Product.products[index].name} ao carrinho')));
+                                  },
+                                  icon: Icon(Icons.add_circle))
+                            ]),
+                      );
+                    })),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed(AppRoutes.CART);
+                  },
+                  child: Text('Check Cart'))
+            ],
+          ),
+        )*/
