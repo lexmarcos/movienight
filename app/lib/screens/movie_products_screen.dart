6eb1ebce187233289d2ca1e3944de3d15f2700d8
henrymedeiros <henrymedeiros77@gmail.com>
2022-06-13 20:48:06 -0300

    melhoria layout pagina de produtos do filme

--- app/lib/screens/movie_products_screen.dart
+++ app/lib/screens/movie_products_screen.dart
@@ -21,8 +21,7 @@ class _MovieProductsScreenState extends State<MovieProductsScreen> {
     if (ModalRoute.of(context)!.settings.arguments == Null) {}
     final arguments = ModalRoute.of(context)!.settings.arguments as Movie;
 
-    Response response =
-        await Api.get('/stores', params: {
+    Response response = await Api.get('/stores', params: {
       'movieID': arguments.id.toString(),
     });
     if (response.statusCode == 200) {
@@ -58,45 +57,50 @@ class _MovieProductsScreenState extends State<MovieProductsScreen> {
 
   Widget renderProducts() {
     if (products != null) {
-      return Column(
-        children: [
-          Flexible(
-            child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: products!.length,
-                itemBuilder: ((context, index) {
-                  return Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 20, vertical: 10),
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          CircleAvatar(
-                              radius: 40,
-                              backgroundImage:
-                                  NetworkImage(products![index].imageUrl)),
-                          Text(products![index].name),
-                          Text('R\$ ${products![index].price}'),
-                          IconButton(
-                              onPressed: () {
-                                ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-                                    content: Text(
-                                        'Adicionou produto ${products![index].name} ao carrinho')));
-                                final product = context.read<CartStore>();
-                                product.addToCart(products![index]);
-                              },
-                              icon: Icon(Icons.add_circle))
-                        ]),
-                  );
-                })),
-          ),
-          ElevatedButton(
-              onPressed: () {
-                Navigator.of(context).pushNamed(AppRoutes.CART);
-              },
-              child: Text('Check Cart'))
-        ],
-      );
+      return Column(children: [
+        Flexible(
+          child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: products!.length,
+              itemBuilder: ((context, index) {
+                return Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        CircleAvatar(
+                            radius: 40,
+                            backgroundImage:
+                                NetworkImage(products![index].imageUrl)),
+                        Text(products![index].name,
+                            style: TextStyle(
+                                fontSize: 16, fontWeight: FontWeight.w400)),
+                        Text(
+                          'R\$ ${products![index].price}',
+                          style: TextStyle(
+                              fontSize: 18, fontWeight: FontWeight.w500),
+                        ),
+                        IconButton(
+                            iconSize: 32,
+                            color: Colors.white,
+                            onPressed: () {
+                              ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                                  content: Text(
+                                      'Adicionou produto ${products![index].name} ao carrinho')));
+                              final product = context.read<CartStore>();
+                              product.addToCart(products![index]);
+                            },
+                            icon: Icon(
+                              Icons.add_circle,
+                              color: Color.fromARGB(255, 255, 17, 0),
+                            ))
+                      ]),
+                );
+              })),
+        ),
+      ]);
     }
     return const Text('Loading products...');
   }
@@ -104,9 +108,23 @@ class _MovieProductsScreenState extends State<MovieProductsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text('Movie Products'),
-        ),
-        body: Center(child: renderProducts()));
+      appBar: AppBar(
+        title: Text('Movie Products'),
+      ),
+      body: Center(child: renderProducts()),
+      bottomNavigationBar: Container(
+          height: 70,
+          child: ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushNamed(AppRoutes.CART);
+              },
+              child: Text(
+                'Check Cart',
+                style: TextStyle(fontSize: 24),
+              ),
+              style: ButtonStyle(
+                  backgroundColor: MaterialStateProperty.all(
+                      Color.fromARGB(255, 255, 17, 0))))),
+    );
   }
 }
